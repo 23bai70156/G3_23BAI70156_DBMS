3a2534b0788bf955e9ccce035991faf19c8b0551
Gaurav Monga <23bai70156@cuchd.in>
2025-12-14 07:43:35 +0530

    Add files via upload

---
+++ G3_23BAI70156_SESSION 2/Session 2 Class Work.sql
@@ -0,0 +1,15 @@
+--G3_23BAI70156_SESSION 2
+
+--Question 1(Leetcode 1661)
+SELECT machine_id,
+ROUND(SUM((CASE WHEN activity_type = 'end' THEN timestamp ELSE -timestamp end)::NUMERIC(10,3))/(COUNT(DISTINCT process_id)),3) AS processing_time
+FROM Activity
+GROUP BY machine_id
+
+
+--Question 2(Leetcode 196)
+DELETE P2
+FROM Person AS P1
+INNER JOIN Person AS P2
+ON P1.email = P2.email
+WHERE P1.id < P2.id;
